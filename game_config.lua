840c4630234e66390b03dcfa9655cebcff50749d
voidlinksbuisness-sudo <voidlinksbuisness@gmail.com>
2026-08-16 15:38:36 -0400

    Update game_config.lua

--- game_config.lua
+++ game_config.lua
@@ -236,7 +236,7 @@ local GameConfig = {
     ["StrikerAnims"] = {
         ["rbxassetid://116642061934550"] = {
             DisplayName = "1stM1",
-            ReactionTime = 0.18,
+            ReactionTime = 0.2,
         },
         ["rbxassetid://115234849770695"] = {
             DisplayName = "2ndM1",
